da6b462b9d76464302ec30bbe02e9233ff729e86
Michael Keiser <m.keiser@bst.ch>
2018-06-19 07:38:20 +0200

    Sample initialized

---
+++ ZbW.Testing.FirstUnitTest.Library.UnitTests/FunctionRepositoryTests.cs
@@ -0,0 +1,16 @@
+﻿namespace ZbW.Testing.FirstUnitTest.Library.UnitTests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FunctionRepositoryTests
+    {
+        [Test]
+        public void TestGreaterThan10()
+        {
+            var functionRepository = new FunctionRepository();
+
+            Assert.IsTrue(functionRepository.GreaterThan10(11));
+        }
+    }
+}
---
+++ ZbW.Testing.FirstUnitTest.Library/FunctionRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ZbW.Testing.FirstUnitTest.Library
+{
+    public class FunctionRepository
+    {
+        public bool GreaterThan10(int value)
+        {
+            return value > 10;
+        }
+    }
+}
